38574801d48b7ca880bb333130d48e28d11b981f
Viviana Salcedo <20243ds058@utez.edu.mx>
2026-04-02 14:14:17 -0600

    Se edito para conectar con back

--- app/src/main/java/com/sigpro/lider/models/LoginRequest.kt
+++ app/src/main/java/com/sigpro/lider/models/LoginRequest.kt
@@ -1,6 +1,12 @@
 package com.sigpro.lider.models
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginRequest(
+
+    @SerializedName("matricula") 
     val matricula: String,
+    
+    @SerializedName("contrasena")
     val contrasena: String
 )
