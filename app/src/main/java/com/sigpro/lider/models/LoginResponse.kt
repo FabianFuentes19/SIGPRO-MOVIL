338119bb15bb436736bfab9092d04848716ec2ee
Viviana Salcedo <20243ds058@utez.edu.mx>
2026-04-08 20:06:40 -0600

    Se añadio la matricula

--- app/src/main/java/com/sigpro/lider/models/LoginResponse.kt
+++ app/src/main/java/com/sigpro/lider/models/LoginResponse.kt
@@ -3,5 +3,6 @@ package com.sigpro.lider.models
 data class LoginResponse(
     val token: String?,
     val rol: String?,
+    val matricula: String?,
     val error: String? // Por si el login falla y el back manda un error
 )
